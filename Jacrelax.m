36e4b069e3747cfc8fbe1f53538c1962403d4a7a
Denis Mulumba <denismulumba@grad-phdcomp-03.boisestate.edu>
2023-05-02 12:34:23 -0600

    Added comments

--- Jacrelax.m
+++ Jacrelax.m
@@ -1,46 +1,62 @@
 function [u, res] = Jacrelax(level, nx, ny, u, f, iters, pre)
+
+% Set pre-processing flag to false
 pre = false;
-dx = 1.0/nx; dy = 1.0/ny;
-Ax = 1.0/dx^2; Ay = 1.0/dy^2;
+
+% Calculate grid spacings in x and y
+dx = 1.0/nx;
+dy = 1.0/ny;
+
+% Calculate constants for discrete Laplacian operator
+Ax = 1.0/dx^2;
+Ay = 1.0/dy^2;
 Ap = 1.0/(2.0*(Ax+Ay));
 
-% Dirichlet BC
+% Apply Dirichlet boundary conditions
 u(1,:) = -u(2,:);
 u(end,:) = -u(end-1,:);
 u(:,1) = -u(:,2);
 u(:,end) = -u(:,end-1);
 
+% If pre-processing is enabled and this is not the coarsest level, 
+% set u to the negative of the right-hand side
 if pre && level > 1
     u(2:nx+1,2:ny+1) = -Ap*f(2:nx+1,2:ny+1);
-    % Dirichlet BC
+    
+    % Apply Dirichlet boundary conditions
     u(1,:) = -u(2,:);
     u(end,:) = -u(end-1,:);
     u(:,1) = -u(:,2);
     u(:,end) = -u(:,end-1);
 
+    % Reduce the number of iterations to account for pre-processing
     iters = iters-1;
 end
 
-  for it = 0:iters+2
-      for i = 2:nx+1
-          for j = 2:ny+1
+% Perform Jacobi relaxation for the specified number of iterations
+for it = 0:iters+2
+    for i = 2:nx+1
+        for j = 2:ny+1
+            % Update u using the discrete Laplacian operator and the right-hand side f
             u(i,j) = Ap*(Ax*(u(i+1,j) + u(i-1,j)) ...
             + Ay*(u(i,j+1) + u(i,j-1)) ...
             - f(i,j));
-          end
-      end
-    % Dirichlet BC
-        u(1,:) = -u(2,:);
-        u(end,:) = -u(end-1,:);
-        u(:,1) = -u(:,2);
-        u(:,end) = -u(:,end-1);
-  end
- res = zeros(nx+2, ny+2);
+        end
+    end
+    
+    % Apply Dirichlet boundary conditions
+    u(1,:) = -u(2,:);
+    u(end,:) = -u(end-1,:);
+    u(:,1) = -u(:,2);
+    u(:,end) = -u(:,end-1);
+end
+
+% Calculate the residual vector
+res = zeros(nx+2, ny+2);
 for i=2:nx+1
     for j=2:ny+1
         res(i,j) = f(i,j) - ((Ax*(u(i+1,j)+u(i-1,j)) + Ay*(u(i,j+1)+u(i,j-1)) - 2.0*(Ax+Ay)*u(i,j)));
     end
 end
-
 
 end
